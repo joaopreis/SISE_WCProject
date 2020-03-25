aca3664e38e7e19442f074491f9ff57763b7e7f7
joaopreis <59958786+joaopreis@users.noreply.github.com>
2020-03-25 15:19:40 +0000

    Add files via upload

---
+++ API/my-server.js
@@ -0,0 +1,100 @@
+const Path = require('path');
+const Hapi = require('hapi');
+const Inert = require('inert');
+
+var fs = require('fs');
+let brands = {}
+
+fs.readFile('brands.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    brands = JSON.parse(data).brands;
+})
+var fs = require('fs');
+let models = {}
+
+fs.readFile('models.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    models = JSON.parse(data).models;
+})
+
+var fs = require('fs');
+let products = {}
+
+fs.readFile('car_products.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+    products = JSON.parse(data).products;
+})
+
+const server = Hapi.Server({
+    host: 'localhost',
+    port: Number(process.argv[2] || 8080)
+});
+
+const init = async () => {
+    try {
+        await server.register(Inert);
+
+        server.route({
+            method: 'GET',
+            path: '/{param*}',
+            handler: {
+                directory: {
+                    path: '../APP/.',
+                    redirectToSlash: true,
+                    index: true,
+                }
+            }
+        });
+
+        await server.start();
+        console.log(`Server running at: ${server.info.uri}`);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+init();
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: (request, h) => {
+        console.log(request);
+        return `Hello ${request.params.name}`;
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/brands',
+    handler: (request, h) => {
+        return (JSON.stringify(brands));
+
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/models',
+    handler: (request, h) => {
+        return (JSON.stringify(models));
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/models/{id}',
+    handler: (request, h) => {
+        return (JSON.stringify(models.filter(m => m.brand_id == request.params.id)));
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/products',
+    handler: (request, h) => {
+        return (JSON.stringify(products));
+    }
+});
